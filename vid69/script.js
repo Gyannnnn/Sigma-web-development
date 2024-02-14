644a943128df545c42669a4872d2e6e60fabbef9
Gyanranjan Patra <hi.gyanaranjanpatra@gmail.com>
2024-02-14 09:29:12 +0530

    dxfg

--- vid69/script.js
+++ vid69/script.js
@@ -1,18 +1,33 @@
-let a = 7
+// let a = 7
 
-function factorial(number){
-   let arr = Array.from(Array(number+1).keys())
-   let c = arr.slice(1,).reduce((a, b)=> a*b )
-   return c
-}
+// function factorial(number){
+//    let arr = Array.from(Array(number+1).keys())
+//    let c = arr.slice(1,).reduce((a, b)=> a*b )
+//    return c
+// }
 
 
-function facFor(number){
-    let fac = 1;
-    for (let index = 1; index <= number; index++) {
-        fac = fac * index
+// function facFor(number){
+//     let fac = 1;
+//     for (let index = 1; index <= number; index++) {
+//         fac = fac * index
+//     }
+//     return fac
+// }
+// console.log(factorial(a))
+// console.log(facFor(a))
+
+// practice
+prompt("Enter a number")
+
+let num = prompt;
+function factorial(num){
+    let facto = 1;
+    for (let index = 1; index <= num ; index++) {
+       facto = facto*index;
+
     }
-    return fac
+    return facto;
 }
-console.log(factorial(a))
-console.log(facFor(a))
+
+console.log(factorial(num));
