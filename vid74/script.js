32ff487268e2bcdd3451d5751d1f38b83b82349d
Gyanranjan Patra <hi.gyanaranjanpatra@gmail.com>
2024-02-15 19:28:38 +0530

    up

---
+++ vid74/script.js
@@ -0,0 +1,14 @@
+let button = document.getElementById("btn");
+
+button.addEventListener("click", ()=>{
+    // alert("Hy Gyanaranjan Patra !");
+    document.querySelector(".container").style.background = "black"
+})
+button.addEventListener("dblclick", ()=>{
+    // alert("Hy Gyanaranjan Patra !");
+    document.querySelector(".container").style.background = "green"
+})
+
+// button.addEventListener("click", ()=>{
+//     document.getElementsByClassName(".container").style.backgroundColor = "green"
+// })
