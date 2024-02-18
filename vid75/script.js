9ba0f65140ae3837581398e3489cbf8d18e0fbca
Gyanranjan Patra <hi.gyanaranjanpatra@gmail.com>
2024-02-18 16:10:00 +0530

    n

---
+++ vid75/script.js
@@ -0,0 +1,10 @@
+console.log("Gyana Is a hacker ")
+console.log("Runa Is a hecker ")
+setTimeout(() => {
+    console.log("Time out function is executed !")
+}, 4000);
+setTimeout(() => {
+    console.log("Time out   iefeiugdsvyue function is executed !")
+}, 4000);
+
+console.log("the End")
