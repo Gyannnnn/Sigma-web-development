3d855d8161f406a8119e0c91b5ec4fa9029be41c
Gyanranjan Patra <hi.gyanaranjanpatra@gmail.com>
2024-03-02 14:39:57 +0530

    learnt new

--- Vid80/script.js
+++ Vid80/script.js
@@ -1,1 +1,53 @@
-console.log("Object Oriented Programming !!");
+// console.log("Object Oriented Programming !!");
+
+
+// let obj  ={
+//     a:1,
+//     b:"Gyanaranjan"
+
+// }
+
+// console.log(obj);
+
+// let animal ={
+//     eats:true
+// }
+// let rabbit={
+//     jumps:true
+// }
+// rabbit.__prototype__ = animal;
+
+
+
+class Animal{
+    constructor(name){
+        this.name = name
+        console.log("Class Is created !!")
+    }
+    eats(){
+        console.log("Kha raha Honn")
+    }
+    jumps(){
+        console.log("kood raha Honn")
+    }
+}
+
+class lion extends Animal{
+    
+    constructor(name){
+        super(name)
+        this.name = name
+        console.log("Class Is created and he is a lion ")
+    }
+    eats(){
+        console.log("Kha raha Honn roar")
+    }  
+
+
+}
+
+let a = new Animal("Bunny");
+console.log(a);
+
+let l = new lion("Shera")
+console.log(l);
