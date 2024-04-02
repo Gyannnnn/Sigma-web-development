424c1154a752120696b6fe1910d4c233f8370466
Gyanranjan Patra <hi.gyanaranjanpatra@gmail.com>
2024-04-02 22:53:57 +0530

    Added DOM

---
+++ DOCUMENT OBJECT MODEL/script.js
@@ -0,0 +1,13 @@
+//  4 PILLARS OF DOM
+// 1 . SECLECTION OF AN Element
+// 2 . CHANGING HTML
+// 3 . CHANGING CSS
+// 4 . EVENT LISTNER
+
+
+var a  = document.querySelector("h1")
+
+
+a.style.color = "brown"
+
+a.style.backgroundColor = "black"
--- vid57/index.js
+++ vid57/index.js
@@ -28,11 +28,34 @@
 //     a--;
 // }while(a>=0)
 
-console.log("I am A Tutorial On Looop")
+// console.log("I am A Tutorial On Looop")
 
 
-let a = 0;
-for (let i = 0 ; i<=4466166; i++){
-    console.log(a);
-    a++;
+// let a = 0;
+// for (let i = 0 ; i<=4466166; i++){
+//     console.log(a);
+//     a++;
+// }
+
+
+const obj = {
+    Name:"Gyanranjan Patra",
+    Branch:"Information Technology",
+    Rollno:"2302080019"
+}
+for (const key in obj) {    
+        const element = obj[key];
+        console.log(key,element)
+        
+    
+}
+
+for (const iterator of "gyanranjan") {
+         console.log(iterator);    
+}
+
+for (const gr of "Gyanaranjan Patra") {
+    console.log(gr)
+    
+    
 }
