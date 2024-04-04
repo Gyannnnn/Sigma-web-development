55db7ce6f88a60bd6de4b065e9a1ba8928745e99
Gyanranjan Patra <hi.gyanaranjanpatra@gmail.com>
2024-04-04 19:07:49 +0530

    updated

--- DOCUMENT OBJECT MODEL/script.js
+++ DOCUMENT OBJECT MODEL/script.js
@@ -5,9 +5,45 @@
 // 4 . EVENT LISTNER
 
 
-var a  = document.querySelector("h1")
+// var a  = document.querySelector("h1")
 
 
-a.style.color = "brown"
+// a.style.color = "brown"
 
-a.style.backgroundColor = "black"
+// a.style.backgroundColor = "black"
+
+
+// a.addEventListener("click",function(){
+//     console.log("Hey I am Clicked");
+//     a.innerHTML = "Badal Gaya Mei tu nikal  "
+//     a.style.color = "pink";
+//     a.style.backgroundColor = "green";
+
+// })
+// a.addEventListener("mouseover", function(event) {
+//     console.log(event.clientX, event.clientY);
+// });
+
+
+
+var bulb = document.querySelector("#bulb");
+var btn = document.querySelector("#btn");
+var click = 0;
+btn.addEventListener("click", function () {
+    if(click == 0){
+        bulb.style.backgroundColor = "yellow";
+        bulb.style.borderStyle = "dotted"
+        bulb.style.boxShadow = "0 0 5px yellow, 0 0 10px yellow, 0 0 100px yellow,0 0 1000px yellow"
+        console.log("clicked")
+        btn.innerHTML = "OFF";
+        click++;
+    }
+    else{
+        bulb.style.backgroundColor = "transparent";
+        bulb.style.borderStyle = "solid";
+        bulb.style.boxShadow = "0 0 0 transparent"
+
+        btn.innerHTML = "ON";
+        click =0;
+    }
+})
