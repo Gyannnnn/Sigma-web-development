1d28ed145d227c11c2ce3a4b9b9ba08c44a23731
Gyanranjan Patra <hi.gyanaranjanpatra@gmail.com>
2024-04-22 16:34:37 +0530

    Update main.js

--- Vid86/main.js
+++ Vid86/main.js
@@ -3,8 +3,8 @@ const hostname = '127.0.0.1';
 const port = 3000;
 const server = createServer((req, res) => {
   res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World');
+  res.setHeader('Content-Type', 'text/html');
+  res.end('<h1>Gyanranjan Patra </h1>');
 });
 server.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
