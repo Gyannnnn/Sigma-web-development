2335e8595944a6c0e2cb039842329159e46d1919
Gyanranjan Patra <hi.gyanaranjanpatra@gmail.com>
2024-05-17 19:21:18 +0530

    map

---
+++ MapFunction/maofunction.js
@@ -0,0 +1,13 @@
+let arr  = [23,45,67,45,2,32,32,32,42,24,2]
+
+// let a = arr.map((e)=>{
+//     console.log(e)
+//     return e+1;
+// })
+// console.log(a)
+let arr2 =[26,2,3,45,6,8,12,45,3,1,90,0,3,2,7,8,]
+
+let a2 = arr2.filter((a)=>{
+    return a>10;
+})
+console.log(a2)
